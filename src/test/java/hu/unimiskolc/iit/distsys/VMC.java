80a63b50fc5ebcddecab94f31fad61f6e8051c0d
KrisztianKis <kiss.kikko@gmail.com>
2015-09-26 12:13:39 +0200

    feladat1.2

--- src/test/java/hu/unimiskolc/iit/distsys/VMC.java
+++ src/test/java/hu/unimiskolc/iit/distsys/VMC.java
@@ -2,7 +2,9 @@ package hu.unimiskolc.iit.distsys;
 
 import hu.mta.sztaki.lpds.cloud.simulator.Timed;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine.ResourceAllocation;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ConstantConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ResourceConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
@@ -13,7 +15,7 @@ public class VMC implements VMCreationApproaches{
 		PhysicalMachine pm1 = ExercisesBase.getNewPhysicalMachine();
 		pm1.turnon();
 		Timed.simulateUntilLastEvent();
-		VirtualAppliance va = new VirtualAppliance("Virtaulappliance1", 34.0, 0);
+		VirtualAppliance va = new VirtualAppliance("Virtaulappliance", 34.0, 0);
 		ResourceConstraints rc1 = new ConstantConstraints(0.25, 10.0, 100);
 		pm1.localDisk.registerObject(va);
 		pm1.requestVM(va, rc1, pm1.localDisk, 2);
@@ -24,12 +26,15 @@ public class VMC implements VMCreationApproaches{
 		PhysicalMachine pm1 = ExercisesBase.getNewPhysicalMachine();
 		pm1.turnon();
 		Timed.simulateUntilLastEvent();
-		VirtualAppliance va = new VirtualAppliance("Virtaulappliance1", 34.0, 0);
-		ResourceConstraints rc1 = new ConstantConstraints(0.25, 10.0, 100);
-		ResourceConstraints rc2 = new ConstantConstraints(0.15, 5.0, 100);
+		VirtualAppliance va = new VirtualAppliance("Virtaulappliance", 34.0, 0);
+		VirtualMachine vm1 = new VirtualMachine(va);
+		VirtualMachine vm2 = new VirtualMachine(va);
+		ResourceAllocation ra1 = pm1.allocateResources(new ConstantConstraints(0.25, 10.0, 100), false, 2);
+		ResourceAllocation ra2 = pm1.allocateResources(new ConstantConstraints(0.25, 10.0, 100), false, 2);
 		pm1.localDisk.registerObject(va);
-		
-		Timed.simulateUntilLastEvent();	
+		pm1.deployVM(vm1, ra1, pm1.localDisk);
+		pm1.deployVM(vm2, ra2, pm1.localDisk);
+		Timed.simulateUntilLastEvent();
 	}
 
 	public void indirectVMCreation() throws Exception{
